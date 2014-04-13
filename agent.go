f1509aba6a2d6eb3a8bdc492bceb54d77cae94c5
Alex Suraci <suraci.alex@gmail.com>
2014-04-13 12:16:40 -0700

    update executable

--- agent.go
+++ agent.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 
+	"github.com/rcrowley/go-tigertonic"
+
 	"github.com/room101-ci/agent/api"
+)
+
+var listenAddr = flag.String(
+	"listen",
+	"0.0.0.0:4637",
+	"listening address",
 )
 
 func main() {
 	logger := log.New(os.Stdout, "", 0)
 	logger.Println("Booting...")
 
-	api := api.New(logger)
-	api.Run()
+	server := tigertonic.NewServer(*listenAddr, api.New(logger))
+
+	err := server.ListenAndServe()
+	logger.Fatalln("listen error:", err)
 }
