65c85e21c8a7449b94eb6c12b86adfa628221565
Alex Suraci <suraci.alex@gmail.com>
2014-04-13 19:15:21 -0700

    add real source fetcher

---
+++ api/builds/sourcefetcher/sourcefetcher.go
@@ -0,0 +1,62 @@
+package sourcefetcher
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pivotal-golang/archiver/extractor"
+
+	"github.com/room101-ci/agent/api/builds"
+)
+
+type SourceFetcher struct {
+	tmpdir string
+
+	extractor extractor.Extractor
+
+	httpClient *http.Client
+}
+
+func NewSourceFetcher(tmpdir string, extractor extractor.Extractor) *SourceFetcher {
+	return &SourceFetcher{
+		tmpdir:    tmpdir,
+		extractor: extractor,
+
+		httpClient: &http.Client{
+			Transport: &http.Transport{},
+		},
+	}
+}
+
+func (fetcher *SourceFetcher) Fetch(source builds.BuildSource) (string, error) {
+	response, err := fetcher.httpClient.Get(source.URI)
+	if err != nil {
+		return "", err
+	}
+
+	tempFile, err := ioutil.TempFile(fetcher.tmpdir, "fetched-file")
+	if err != nil {
+		return "", err
+	}
+
+	_, err = io.Copy(tempFile, response.Body)
+	if err != nil {
+		tempFile.Close()
+		return "", err
+	}
+
+	tempFile.Close()
+
+	tempDir, err := ioutil.TempDir(fetcher.tmpdir, "fetched-contents")
+	if err != nil {
+		return "", err
+	}
+
+	err = fetcher.extractor.Extract(tempFile.Name(), tempDir)
+	if err != nil {
+		return "", err
+	}
+
+	return tempDir, nil
+}
---
+++ api/builds/sourcefetcher/sourcefetcher_test.go
@@ -0,0 +1,127 @@
+package sourcefetcher_test
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/ghttp"
+	Extractor "github.com/pivotal-golang/archiver/extractor"
+	"github.com/pivotal-golang/archiver/extractor/fake_extractor"
+
+	"github.com/room101-ci/agent/api/builds"
+	. "github.com/room101-ci/agent/api/builds/sourcefetcher"
+	"github.com/room101-ci/agent/testhelpers/archiver"
+)
+
+var _ = Describe("SourceFetcher", func() {
+	var tmpdir string
+	var extractor Extractor.Extractor
+	var sourceFetcher *SourceFetcher
+
+	var server *ghttp.Server
+
+	BeforeEach(func() {
+		var err error
+
+		tmpdir, err = ioutil.TempDir("", "agent-source-fetcher")
+		Ω(err).ShouldNot(HaveOccurred())
+
+		server = ghttp.NewServer()
+
+		extractor = Extractor.NewDetectable()
+	})
+
+	JustBeforeEach(func() {
+		sourceFetcher = NewSourceFetcher(tmpdir, extractor)
+	})
+
+	Describe("fetching a raw source", func() {
+		Context("when the url is a .tar.gz", func() {
+			var archivePath string
+			var source builds.BuildSource
+
+			BeforeEach(func() {
+				tmpfile, err := ioutil.TempFile("", "agent-source-fetcher-tgz")
+				Ω(err).ShouldNot(HaveOccurred())
+
+				archivePath := tmpfile.Name()
+
+				os.Remove(archivePath)
+
+				archiver.CreateTarGZArchive(archivePath, []archiver.ArchiveFile{
+					{
+						Name: "some-file",
+						Body: "some-file-contents",
+					},
+				})
+
+				source = builds.BuildSource{
+					Type: "raw",
+					URI:  server.URL() + "/foo.tar.gz",
+				}
+
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/foo.tar.gz"),
+						func(w http.ResponseWriter, _ *http.Request) {
+							w.WriteHeader(http.StatusOK)
+
+							archive, err := os.Open(archivePath)
+							Ω(err).ShouldNot(HaveOccurred())
+
+							_, err = io.Copy(w, archive)
+							Ω(err).ShouldNot(HaveOccurred())
+						},
+					),
+				)
+			})
+
+			AfterEach(func() {
+				os.Remove(archivePath)
+			})
+
+			It("downloads it into the temporary directory", func() {
+				fetched, err := sourceFetcher.Fetch(source)
+				Ω(err).ShouldNot(HaveOccurred())
+
+				body, err := ioutil.ReadFile(filepath.Join(fetched, "some-file"))
+				Ω(err).ShouldNot(HaveOccurred())
+
+				Ω(string(body)).Should(Equal("some-file-contents"))
+			})
+
+			Context("when downloading fails", func() {
+				BeforeEach(func() {
+					server.Close()
+				})
+
+				It("returns an error", func() {
+					_, err := sourceFetcher.Fetch(source)
+					Ω(err).Should(HaveOccurred())
+				})
+			})
+
+			Context("when extracting fails", func() {
+				disaster := errors.New("oh no!")
+
+				BeforeEach(func() {
+					fakeExtractor := &fake_extractor.FakeExtractor{}
+					fakeExtractor.SetExtractOutput(disaster)
+
+					extractor = fakeExtractor
+				})
+
+				It("returns the error", func() {
+					_, err := sourceFetcher.Fetch(source)
+					Ω(err).Should(Equal(disaster))
+				})
+			})
+		})
+	})
+})
