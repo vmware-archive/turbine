90f1be3d5d6c38d76771898a0be863987f0f0556
Alex Suraci <suraci.alex@gmail.com>
2014-06-23 16:03:39 -0700

    deps

--- Godeps/_workspace/src/github.com/onsi/ginkgo/types/types_suite_test.go
+++ Godeps/_workspace/src/github.com/onsi/ginkgo/types/types_suite_test.go
@@ -1,4 +1,4 @@
-package transport_test
+package types_test
 
 import (
 	. "github.com/onsi/ginkgo"
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestTransport(t *testing.T) {
+func TestTypes(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Transport Suite")
+	RunSpecs(t, "Types Suite")
 }
