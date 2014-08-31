5ee58ad525e413c959d7930b9456aff12b0fc2dd
Alex Suraci <suraci.alex@gmail.com>
2014-08-30 17:49:08 -0700

    logwriter emits valid utf8
    [#73309626]

--- logwriter/logwriter.go
+++ logwriter/logwriter.go
@@ -2,6 +2,7 @@ package logwriter
 
 import (
 	"io"
+	"unicode/utf8"
 
 	"github.com/concourse/turbine/event"
 )
@@ -9,6 +10,8 @@ import (
 type writer struct {
 	emitter event.Emitter
 	origin  event.Origin
+
+	dangling []byte
 }
 
 func NewWriter(emitter event.Emitter, origin event.Origin) io.Writer {
@@ -19,8 +22,18 @@ func NewWriter(emitter event.Emitter, origin event.Origin) io.Writer {
 }
 
 func (writer *writer) Write(data []byte) (int, error) {
+	text := append(writer.dangling, data...)
+
+	checkEncoding, _ := utf8.DecodeLastRune(text)
+	if checkEncoding == utf8.RuneError {
+		writer.dangling = text
+		return len(data), nil
+	}
+
+	writer.dangling = nil
+
 	writer.emitter.EmitEvent(event.Log{
-		Payload: data,
+		Payload: text,
 		Origin:  writer.origin,
 	})
 
